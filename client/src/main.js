03030e0bd3b44a0dc3f7e210e6467450de7c7036
Nikhil <nsahu@argusoft.com>
2023-11-01 01:37:39 +0530

    created login using vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -5,8 +5,20 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
 import 'bootstrap/dist/js/bootstrap.js';
 import routes from './routes.js'
+import VeeValidate from 'vee-validate';
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 const app = createApp(App)
+app.use(vuetify)
+app.use(VeeValidate)
 app.use(store)
 app.use(routes)
 app.mount('#app')
--- client/src/views/LoginPage.vue
+++ client/src/views/LoginPage.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
 
 <div id="container">
     <form @submit.prevent="handleLogin()">
@@ -55,60 +55,103 @@ export default{
 
 <style scoped>
 
-* {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-    font-family: Raleway, sans-serif;
+</style> -->
 
-}
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" sm="8" md="5">
+        <v-card elevation="2">
+          <v-card-title class="headline text-center">Login</v-card-title>
+          <v-card-text>
+            <v-form @submit.prevent="handleLogin()">
+              <v-text-field
+                label="Username"
+                v-model="username"
+                :rules="usernameRules"
+                required
+              ></v-text-field>
+              <v-text-field
+                label="Password"
+                v-model="password"
+                :rules="passwordRules"
+                :type="showPassword ? 'text' : 'password'"
+                @click:append="showPassword = !showPassword"
+                required
+              ></v-text-field>
+              <!-- <v-card-text v-if="emptyPassword">password field cannot be empty</v-card-text> -->
+              <v-card-text v-if="!isValidUser"
+                >Invalid username or password</v-card-text
+              >
+              <v-card-text>{{ errMessage }}</v-card-text>
+              <v-btn class="ma-2 float-right" color="primary" type="submit"
+                >Login</v-btn
+              >
+            </v-form>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
 
-#container{
-    margin-top:5vw;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}   
+<script>
+import { mapActions } from "vuex";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email } from "@vuelidate/validators";
+import "@mdi/font/css/materialdesignicons.css"; // Import MDI icons
+export default {
+  setup() {
+    return { v$: useVuelidate() };
+  },
+  data() {
+    return {
+      username: "",
+      password: "",
+      showPassword: false,
+      isValidUser: true,
+      emptyPassword: false,
+      errMessage: "",
+      usernameRules: [
+        (v) => !!v || "Username is required",
+        (v) =>
+          /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(v) ||
+          "Invalid email format",
+      ],
+      passwordRules: [(v) => !!v || "password is required"],
+    };
+  },
+  validations() {
+    return {
+      username: { required, email },
+      password: { required },
+    };
+  },
+  methods: {
+    ...mapActions("admin", ["login"]),
 
-form{
-    width:30vw;
-    min-height:50vh;
-    padding:2vw 3vw;
-    box-shadow: 0px 0px 24px #c8c6de;
-}
-
-label{
-    margin-bottom: 0.5vw;
-}
-.form-group{
-    margin-bottom:1vw;
-}
-
-input{
-    padding:5px 10px;
-}
-
-#error{
-    color:red
-}
-
-.error{
-    color:red;
-    margin-top:-10px;
-}
-
-
-#heading{
-    text-align: center;
-    margin-bottom:2vw;
-}
-
-#heading h1{
-    font-weight: 600
-}
-
-button{
-    padding:8px 15px;
-    float:right;
-}
-</style>
+    async handleLogin() {
+      // this check if username is not empty and password field is empty when the user clicks the login button
+      this.emptyPassword =this.v$.password.$invalid &&(await this.v$.username.$validate()) === true;
+      this.isValidCredential = await this.v$.$validate()
+      if (!this.isValidCredential) {
+        console.log("failure");
+        this.clearForm();
+        return;
+      }
+      const response = await this.login("hey");
+      console.log(response)
+      this.errMessage = response;
+    //   this.clearForm();
+    },
+    async login() {},
+    // togglePasswordVisibility() {
+    //   this.showPassword = !this.showPassword;
+    // },
+    clearForm() {
+      this.username = "";
+      this.password = "";
+    },
+  },
+};
+</script>
